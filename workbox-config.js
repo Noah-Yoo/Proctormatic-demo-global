1ce5f405ee43f14e288640129d48c54cf3eac8ae
Noah-Yoo <mhyoo@edint.io>
2024-06-03 18:03:04 +0900

    프리캐싱 변경

--- workbox-config.js
+++ workbox-config.js
@@ -8,53 +8,53 @@ module.exports = {
     // './media/combined_video.mp4'
   ],
   swDest: 'service-worker.js',
-  swSrc: 'src-sw.js',
+  // swSrc: 'src-sw.js',
   modifyURLPrefix: {
     '': '/Proctormatic-demo-global/'  // 여기에 리포지토리 이름을 입력합니다.
   },
 
   maximumFileSizeToCacheInBytes: 80 * 1024 * 1024, // 80MB
 
-  // runtimeCaching: [
-  //   {
-  //     urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:png|jpg|jpeg|svg)$'),
-  //     handler: 'CacheFirst',
-  //     options: {
-  //       cacheName: 'images',
-  //       expiration: {
-  //         maxEntries: 100,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:mp4)$'),
-  //     handler: 'CacheFirst',
-  //     options: {
-  //       cacheName: 'videos',
-  //       expiration: {
-  //         maxEntries: 5,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:woff2)$'),
-  //     handler: 'CacheFirst',
-  //     options: {
-  //       cacheName: 'fonts',
-  //       expiration: {
-  //         maxEntries: 20,
-  //       },
-  //     },
-  //   },
-  //   {
-  //     urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:js|css|json|html)$'),
-  //     handler: 'StaleWhileRevalidate',
-  //     options: {
-  //       cacheName: 'static-resources',
-  //       expiration: {
-  //         maxEntries: 60,
-  //       },
-  //     },
-  //   }
-  // ],
+  runtimeCaching: [
+    {
+      urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:png|jpg|jpeg|svg)$'),
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 100,
+        },
+      },
+    },
+    {
+      urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:mp4)$'),
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'videos',
+        expiration: {
+          maxEntries: 5,
+        },
+      },
+    },
+    {
+      urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:woff2)$'),
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'fonts',
+        expiration: {
+          maxEntries: 20,
+        },
+      },
+    },
+    {
+      urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:js|css|json|html)$'),
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-resources',
+        expiration: {
+          maxEntries: 60,
+        },
+      },
+    }
+  ],
 };
