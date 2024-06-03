ac61415ad4352f6dee3394f569a7ffe94fcc2067
Noah-Yoo <mhyoo@edint.io>
2024-06-03 17:57:59 +0900

    캐싱 정책 변경

--- workbox-config.js
+++ workbox-config.js
@@ -2,12 +2,19 @@ module.exports = {
   globDirectory: './',
   globPatterns: [
     '**/*.{html,js,css,json,svg,png,woff2,mp4}'
+    // './media/reportSpecific/0/0/combined_video.mp4',
+    // './media/reportSpecific/0/1/combined_video.mp4',
+    // './img/arrow.svg'
+    // './media/combined_video.mp4'
   ],
   swDest: 'service-worker.js',
   swSrc: 'src-sw.js',
   modifyURLPrefix: {
     '': '/Proctormatic-demo-global/'  // 여기에 리포지토리 이름을 입력합니다.
   },
+
+  maximumFileSizeToCacheInBytes: 80 * 1024 * 1024, // 80MB
+
   // runtimeCaching: [
   //   {
   //     urlPattern: new RegExp('/Proctormatic-demo-global/.*\\.(?:png|jpg|jpeg|svg)$'),
