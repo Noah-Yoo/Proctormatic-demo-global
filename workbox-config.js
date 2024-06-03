816e55b30e4a2b042ae9b2a3d3d784d8d520e35f
Noah-Yoo <mhyoo@edint.io>
2024-06-03 20:43:45 +0900

    수정

--- workbox-config.js
+++ workbox-config.js
@@ -4,22 +4,11 @@ module.exports = {
     '**/*.{html,js,css,png,svg,json,woff2,mp4}'
   ],
   swDest: 'service-worker.js',
-  clientsClaim: true,
-  skipWaiting: true,
+  swSrc: 'src-sw.js',
+  // clientsClaim: true,
+  // skipWaiting: true,
   modifyURLPrefix: {
     '': '/Proctormatic-demo-global/'  // 여기에 리포지토리 이름을 입력합니다.
   },
   maximumFileSizeToCacheInBytes: 80 * 1024 * 1024, // 80MB
-  runtimeCaching: [
-    {
-      urlPattern: ({ request }) => true, // 모든 요청을 캐시
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'precache-v1',
-        expiration: {
-          maxEntries: 100,
-        },
-      },
-    },
-  ],
 };
